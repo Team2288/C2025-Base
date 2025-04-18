a85938c8b83cb8c954b8ab40293b4dea183628e6
FurtiveSword <128654614+FurtiveSword@users.noreply.github.com>
2025-04-17 17:26:44 -0700

    Reset added the subsystems back

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "C2025-Base-main-Imported";
+  public static final String MAVEN_NAME = "C2025-Base";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "b217438b1f2a506bc41b01e18684744f8a993a60";
-  public static final String GIT_DATE = "2025-03-11 20:55:07 EDT";
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "a5f399b4016ec2f7eba0202c4fd24a97a573183a";
+  public static final String GIT_DATE = "2025-04-08 20:44:03 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-04-05 17:13:08 EDT";
-  public static final long BUILD_UNIX_TIME = 1743887588223L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-04-15 19:54:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1744761243991L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
